c48259ac36feaf2aa570a55c75954956d9185b37
Saurabh Yadav <saurabhyadav970@gmail.com>
2021-06-19 16:47:53 +0530

    First Commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./topbar.scss"
+import {Mail} from "@material-ui/icons"
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className = "logo">
+                        Saurabh Yadav
+                    </a>
+                    <div className="itemContainer">
+                        <Mail className = "icon"/>
+                        <span>saurabhyadav970@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick = {()=> setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
